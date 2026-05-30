e1215e8a0270369974c0322c84547e14f5b34cd4
Khaledll9 <khaledalkady779@gmail.com>
2026-05-30 12:00:17 +0300

    extract TasksService and CardComponent, wire up new-task form with two-way binding

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,19 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
 import { type Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
+import { TasksService } from '../tasks.service';
 
 @Component({
-  selector: 'app-task',
-  standalone: true,
-  imports: [],
-  templateUrl: './task.component.html',
-  styleUrl: './task.component.css',
+    selector: 'app-task',
+    standalone: true,
+    templateUrl: './task.component.html',
+    styleUrl: './task.component.css',
+    imports: [CardComponent, DatePipe]
 })
 export class TaskComponent {
-  @Input({ required : true }) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+  @Input({required: true}) task!: Task;
+  private tasksService = inject(TasksService);
 
-
-  onComplete(){
-    this.complete.emit(this.task.id);
+  onCompleteTask() {
+    this.tasksService.removeTask(this.task.id);
   }
 }
